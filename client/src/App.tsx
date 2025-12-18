14171d4b6dc5f3cacbb5f1543f35c75618aac0df
Muhaimen-48 <141935800+Muhaimen-48@users.noreply.github.com>
2025-12-18 21:46:08 +0600

    Remove CV route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,12 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
-import CVPage from "@/pages/cv";
 import ResearchProjectPage from "@/pages/research/[id]";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/cv" component={CVPage} />
       <Route path="/research/:id" component={ResearchProjectPage} />
       <Route component={NotFound} />
     </Switch>
